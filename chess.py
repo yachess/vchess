327a0e3cb6c9f946d6d34eeafb7a09101a186b35
Youngjin Kim <yachess@gmail.com>
2021-01-15 13:44:34 +0900

    ui enhanced

--- chess.py
+++ chess.py
@@ -1,6 +1,7 @@
 import copy
 
 init_pos = "rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR"
+init_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 q_atks=[]
 b_atks=[]
@@ -34,7 +35,7 @@ for i in range(64):
     wp_atks.append([])
 
 # list indices
-#  0 - 1 - 2
+#  0   1   2
 #    \ | / 
 #  3 -sqr- 4
 #    / | \
@@ -161,15 +162,23 @@ class Move:
         return "{}>{}".format(sqr_to_pgn(self.src),sqr_to_pgn(self.dst))
 
 # Castling rights constants
-cr_K=0b0001
-cr_Q=0b0010
-cr_k=0b0100
-cr_q=0b1000
+#r_K=0b0001
+#r_Q=0b0010
+#r_k=0b0100
+#r_q=0b1000
+
+cr_K= 1 << 0
+cr_Q= 1 << 1
+cr_k= 1 << 2
+cr_q= 1 << 3
 
 class Chess:
-    def __init__(self, fen = None):
+    def __init__(self):
         print("init")
         self.bb = {}
+        self._fire_action = None
+
+    def setup(self, fen = None):
         self.t = 1
         self.ply = 0
         self.crights = cr_K | cr_Q | cr_k | cr_q
@@ -177,10 +186,34 @@ class Chess:
         # initialize bitboards
         for p in 'prnbqkrPRNBQKR':
             self.bb[p]=0
-        for i in range(len(init_pos)):
-            if init_pos[i] != '.':
-                self.bb[init_pos[i]] |= 1 << i
-         
+
+        if fen == None:
+            fen = init_fen
+        i=0
+        tokens = fen.split()
+        for c in tokens[0]:
+            if c.isalpha():
+                self.bb[c] |= 1 << i
+                i+=1
+            elif c.isdigit():
+                i+=int(c)
+        self.t = 1 if tokens[1] == "w" else "b"
+    
+        self.crights = 0
+        if "K" in tokens[2]:
+            self.crights |= cr_K
+        if "Q" in tokens[2]:
+            self.crights |= cr_Q
+        if "k" in tokens[2]:
+            self.crights |= cr_k
+        if "q" in tokens[2]:
+            self.crights |= cr_q
+        
+        self.ep_sqr = None if tokens[3]=="-" else tokens[3]
+
+        if self._fire_action is not None:
+            self._fire_action("<<setup>>",fen)
+
     def print(self):
         for sq in range(64):
             b = 1 << sq
@@ -193,15 +226,46 @@ class Chess:
                 print(" .", end='')
             if sq % 8 == 7:
                 print("")
-        pass
 
+    def fen(self):
+        # encode FEN notation
+        l=[]
+        spc = 0
+        for sq in range(64):
+            b = 1 << sq
+            found = False
+            for p in "prnbqkPRNBQK":
+                if self.bb[p] & b:
+                    l.append(p)
+                    found = True
+            if not found:
+                spc += 1
+            if sq % 8 == 7 and sq != 63:
+                if spc > 0:
+                    l.append(str(spc))
+                    spc = 0
+                l.append("/")
+        l.append(" ")
+        l.append("b" if self.t == 0 else "w")
+        l.append(" ")
+        if self.crights == 0:
+            l.append ("-")
+        else:
+            l.append("K" if self.crights & cr_K else "")
+            l.append("Q" if self.crights & cr_Q else "")
+            l.append("k" if self.crights & cr_k else "")
+            l.append("q" if self.crights & cr_q else "")
+        l.append(" ")
+        l.append("-" if self.ep_sqr == None else sqr_to_pgn(self.ep_sqr))
+        l.append(" 0 ")
+        l.append(str(self.ply+1))
+        return "".join(l)
+        
     def put(self,sq,pc):
         self.bb[pc] |= 1 << sq
-        pass
 
     def remove(self,sq,pc):
         self.bb[pc] &= ~(1 << sq)
-        pass
 
     def get_all_moves(self):
         occ = [0,0]
@@ -356,37 +420,40 @@ class Chess:
             elif mv.src==4 and mv.dst==2:
                 self.move('r',0,3)
             self.crights &= ~(cr_k|cr_q)
-        elif piece is 'K':
+        elif piece == 'K':
             if mv.src==60 and mv.dst==62:
                 self.move('R',63,61)
             elif mv.src==60 and mv.dst==58:
                 self.move('R',56,59)
             self.crights &= ~(cr_K|cr_Q)
         #promotion
-        elif piece is 'p':
+        elif piece == 'p':
             if mv.dst // 8 == 7:
                 pass
             elif mv.dst == self.ep_sqr:  #ep capture
                 self.remove('p', mv.dst - 8)
-        elif piece is 'P':
+        elif piece == 'P':
             if mv.dst // 8 == 0:
                 pass
             elif mv.dst == self.ep_sqr:
                 self.remove('p', mv.dst + 8)
         #ep square set/unset
-        if piece is 'p' and mv.dst - mv.src == 16:  #ep-sqr set
+        if piece == 'p' and mv.dst - mv.src == 16:  #ep-sqr set
             self.ep_sqr = mv.src + 8
-        elif piece is 'P' and mv.src - mv.dst == 16:
+        elif piece == 'P' and mv.src - mv.dst == 16:
             self.ep_sqr = mv.src - 8
         else:
             self.ep_sqr = None
         self.t ^= 1
         self.ply += 1
 
+
 if __name__=="__main__":
     build_atk_maps()
     chess = Chess()
+    chess.setup()
 
+    print(chess.fen())
     while True:
         chess.print()
         mvs = chess.get_all_moves()
--- chessui.py
+++ chessui.py
@@ -1,21 +1,43 @@
-
 import tkinter as tk
 import chess
 
+cs = 80
 class ChessBoard(tk.Canvas):
 
     def __init__(self, master = None):
-        super().__init__(master,width = 64*8, height = 64*8)
+        super().__init__(master,width = cs*8, height = cs*8)
         self.master = master
-        
+        self.pieces = {}        
         for sq in range(64):
             i,j = sq % 8, sq // 8
-            x,y = i * 64, j * 64
-            self.create_rectangle(
-                    x, y, x+64, y+64, width=0, 
+            x,y = i * cs, j * cs
+            =self.create_rectangle(
+                    x, y, x+cs, y+cs, width=0, 
                     fill= "green" if (i+j)%2 == 1 else "light green")
-        self.create_image(32, 32, image=pieces['P'], anchor="center")
+#       self.create_image(cs/2, cs/2, image=pieces['P'], anchor="center")
         self.pack()
+
+    def setup (self, fen):
+        tokens = fen.split()[0]
+        i=0
+        for c in tokens:
+            if c.isalpha():
+                x, y = i%8, i//8
+                self.pieces[i]=self.create_image(x*cs+cs/2, y*cs+cs/2, image=pieces[c],
+                anchor="center")
+                i+=1
+            elif c.isdigit():
+                i+=int(c)
+
+    def set (self, pc, sq):
+        x, y = sq%8, sq//8
+        self.pieces[i]=self.create_image(x*cs+cs/2, y*cs+cs/2,
+                image=pieces[pc], anchor="center")
+    
+    def remove (self, sq):
+        i = sq // 8 + sq % 8
+        self.remove(self.pieces[i])
+        del self.pieces[i]
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -26,8 +48,10 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         self.chessboard = ChessBoard(self)
-        self.chess = chess.Chess(self)
-        self.chess._fire_action= self._action
+        self.chess = chess.Chess()
+        self.chess._fire_action = self._handle_action
+        self.chess.setup() 
+
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
@@ -40,8 +64,12 @@ class Application(tk.Frame):
     def say_hi(self):
         print("hi there, everyone!")
 
-    def _action(self, action, args):
-        if action == "<<move>>":
+    def _handle_action(self, action, args):
+        print(action)
+        if action == "<<setup>>":
+            self.chessboard.setup(args)
+            pass
+        elif action == "<<move>>":
             pass
         elif action == "<<move>>":
             print("<<move>>")
@@ -50,10 +78,39 @@ class Application(tk.Frame):
             print("<<remove>>")
             pass
         pass
+
+
+from PIL import Image, ImageEnhance, ImageTk
+
+
+
+def resize_image(img, w, h):
+    img2 = img.resize((w,h), Image.ANTIALIAS)
+    return img2
+
+def darker_image(img, ratio):
+    pxls = img.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            alpha = ratio   # 0..1 :  0 is darkest
+            beta = -30
+            pxls[i,j] = (
+                    int(pxls[i,j][0] * alpha + beta),
+                    int(pxls[i,j][1] * alpha + beta),
+                    int(pxls[i,j][2] * alpha + beta),
+                    pxls[i,j][3])
+    return img
+import PIL
+from PIL import Image
+
 root = tk.Tk()
 pieces={}
 for a in "PRNBQK":
-    pieces[a] = tk.PhotoImage(file = "data/PIECE/Mono/{}.png".format(a))
+#   pieces[a] = tk.PhotoImage(file = "data/PIECE/Dyche/{}.png".format(a))
+    img = resize_image(Image.open("data/PIECE/Dyche/{}.png".format(a)), cs, cs)
+    pieces[a] = ImageTk.PhotoImage(img)
+    pieces[a.lower()] = ImageTk.PhotoImage(darker_image(img, 0.5))
+
 app = Application(master=root)
 app.mainloop()
 
